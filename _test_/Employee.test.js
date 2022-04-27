e48fe2f6acd60409b66c124430a8b178c9b82023
Ballard Ingram <ballardingram@outlook.com>
2022-04-27 13:59:06 -0500

    employee.test - created return tests

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -1,1 +1,28 @@
-const employee = require("../lib/Engineer");
+// DOCUMENATION > NODE CLASSES (https://www.geeksforgeeks.org/how-to-use-class-in-node-js/)
+const employee = require("../lib/Employee");
+
+employee = class {
+    constructor(name=null, id=null, email=null) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = "Employee";
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return this.role
+    }
+
+}
