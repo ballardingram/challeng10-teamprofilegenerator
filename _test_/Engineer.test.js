25ae4d7ff0327170bb602c92809a804104285da7
Ballard Ingram <ballardingram@outlook.com>
2022-04-29 10:54:02 -0500

    engineer testing objects - passed

--- _test_/Engineer.test.js
+++ _test_/Engineer.test.js
@@ -1,1 +1,10 @@
-const employee = require("../lib/Employee");
+const Engineer = require('../lib/Engineer');
+const engineer = new Engineer('Bob', '123456789', 'bob@email.com', 'github.com/bobtest');
+
+// TEST DETAIL > This will match the 'new Engineer' set up to the expected outcomes.
+test('engineer objects >  test constructior values', () => {
+    expect(engineer.name).toBe('Bob');
+    expect(engineer.id).toBe('123456789');
+    expect(engineer.email).toBe('bob@email.com');
+    expect(engineer.github_username).toBe('github.com/bobtest');
+});
