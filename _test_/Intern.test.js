fad678cf36171e7199052122b392d67bcec1feca
Ballard Ingram <ballardingram@outlook.com>
2022-04-29 11:33:23 -0500

    intern.js - passed all test, fixed syntax issues during errors with npm test

--- _test_/Intern.test.js
+++ _test_/Intern.test.js
@@ -1,1 +1,27 @@
-const employee = require("../lib/Intern");
+const Intern = require("../lib/Intern");
+const intern = new Intern('Bob', '123456789', 'bob@email.com', 'Test University');
+
+// TEST DETAIL > This will match the 'new Intern' set up to the expected outcomes.
+test('intern objects > test constructor values', () => {
+    expect(intern.name).toBe('Bob');
+    expect(intern.id).toBe('123456789');
+    expect(intern.email).toBe('bob@email.com');
+    expect(intern.school).toBe('Test University');
+});
+
+// TEST DETAIL > This section will test the 'getObject' method.
+test('name object > test if getName method works', () => {
+    expect(intern.getName()).toBe('Bob');
+});
+
+test('id object > test if getId method works', () => {
+    expect(intern.getId()).toBe('123456789');
+});
+
+test('email object > test if getEmail method works', () => {
+    expect(intern.getEmail()).toBe('bob@email.com');
+});
+
+test('school object > test if getSchool method works', () => {
+    expect(intern.getSchool()).toBe('Test University');
+});
--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -2,7 +2,6 @@ const Manager = require("../lib/Manager");
 const manager = new Manager('Bob', '123456789', 'bob@email.com', '512-512-5112');
 
 // TEST DETAIL > This will match the 'new Manager' set up to the expected outcomes.
-
 test('manager objects > test constructor values', () => {
     expect(manager.name).toBe('Bob');
     expect(manager.id).toBe('123456789');
@@ -11,7 +10,6 @@ test('manager objects > test constructor values', () => {
 });
 
 // TEST DETAIL > This section will test the 'getObject' method.
-
 test('name object > test if getName method works', () => {
     expect(manager.getName()).toBe('Bob');
 });
