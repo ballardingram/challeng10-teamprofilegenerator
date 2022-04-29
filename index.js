e9fe759a7a90e3e09972096b92c900bd005d5ca6
Ballard Ingram <ballardingram@outlook.com>
2022-04-29 14:07:28 -0500

    prompts.js file - existing code, redoing this using old code from challenge 9 in next commit

--- index.js
+++
@@ -1,6 +0,0 @@
-// EXTERNAL PACKAGES > INQUIRER AND JEST INSTALLED
-// DOCUMENTATION > INQUIRER (https://www.npmjs.com/package/inquirer)
-// DOCUMENTATION > JEST (https://www.npmjs.com/package/jest)
-
-const inquirer = require('inquirer');
-const jest = require ('jest');
--- src/prompts.js
+++ src/prompts.js
@@ -1,6 +1,10 @@
-// EXTERNAL PACKAGES > INQUIRER INSTALLED
+// EXTERNAL PACKAGES > INQUIRER INSTALLED; FS AND PATH DO NOT REQUIRE INSTALL
 // DOCUMENTATION > NODE INQUIRER (https://github.com/SBoudrias/Inquirer.js)
 const inquirer = require('inquirer');
+const fs = require("fs");
+const path = require("path");
+//const index = require('../dist/index');
+
 
 // ROUTES > TO OTHER PAGES REQUIRED FOR PROMPTS TO FUNCTION
 const Engineer = require("../lib/Engineer");
@@ -8,63 +12,42 @@ const Manager = require("../lib/Manager");
 const Intern = require("../lib/Intern");
 
 // CREATING AN EMPLOYEES ARRAY
-let employees = [];
+const employees = [];
 
-// CONST > PROMPTS FOR APPLICATION
-const groupPrompts = () => {
-    prompt ([
+// PROMPTS > MANAGER
+const promptManager = () => {
+    return inquirer.prompt ([
         {
-            type: "input",
-            name: "name",
-            message: 'What is the employee name?',
+           type: "input" ,
+           name: "name",
+           message: "What is the employee's name?",
         },
         {
             type: "input",
             name: "id",
-            message: 'What is the employee ID number?',
+            mesage: "What is the employee ID number? MAX 9 Numbers!",
         },
         {
             type: "input",
             name: "email",
-            message: 'What is the employee email address?',
+            message: "What is the employee's email address?",
         },
-    ]);
-};
-
-const engineerPrompts = () => {
-    prompt ([
-        {
-            type: "input",
-            name: "github_username",
-            message: 'What is the github User Name?',
-        },
-    ]);
-};
-
-const managerPrompts = () => {
-    prompt ([
         {
             type: "input",
             name: "officeNumber",
-            message: 'What is the office number?',
+            message: "What is the employee's office number?",
         },
-    ]);
-};
-
-const internPrompts = () => {
-    prompt ([
-        {
-            type: "input",
-            name: "school",
-            message: 'What school did/are they attend/ing?',
-        },
-    ]);
+    ])
 };
 
 // FUNCTION > INITIALIZE PROGRAM
 function init() {
-    inquirer.prompt(groupPrompts)
-    .then(prompt(engineerPrompts) {
-        console.log()
+    inquirer.prompt(promptManager)
+    .then(function(data) {
+        fs.writeFile("Employee-Dashboard.html", index(data));
+        console.log(data)
     })
 }
+
+// FUNCTION > CALL TO INITIALIZE PROGRAM
+init();
