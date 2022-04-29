9728580a83b8b7454ce531840e526cf6dd6f375b
Ballard Ingram <ballardingram@outlook.com>
2022-04-29 11:14:28 -0500

    manager test - passed objects test

--- _test_/Engineer.test.js
+++ _test_/Engineer.test.js
@@ -2,7 +2,8 @@ const Engineer = require('../lib/Engineer');
 const engineer = new Engineer('Bob', '123456789', 'bob@email.com', 'github.com/bobtest');
 
 // TEST DETAIL > This will match the 'new Engineer' set up to the expected outcomes.
-test('engineer objects >  test constructior values', () => {
+
+test('engineer objects >  test constructor values', () => {
     expect(engineer.name).toBe('Bob');
     expect(engineer.id).toBe('123456789');
     expect(engineer.email).toBe('bob@email.com');
--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -1,1 +1,11 @@
-const employee = require("../lib/Manager");
+const Manager = require("../lib/Manager");
+const manager = new Manager('Bob', '123456789', 'bob@email.com', '512-512-5112');
+
+// TEST DETAIL > This will match the 'new Manager' set up to the expected outcomes.
+
+test('manager objects > test constructor values', () => {
+    expect(manager.name).toBe('Bob');
+    expect(manager.id).toBe('123456789');
+    expect(manager.email).toBe('bob@email.com');
+    expect(manager.officeNumber).toBe('512-512-5112');
+});
