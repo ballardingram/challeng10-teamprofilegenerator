46639aacee680dc183fcea9fa115c296080ec7f3
Ballard Ingram <ballardingram@outlook.com>
2022-05-05 09:29:35 -0500

    removed prompts.js, no longer needed as sandbox, added comments to index.js

--- index.js
+++ index.js
@@ -4,19 +4,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require("path");
+const generateHTML =require("./src/generateHTML");
 
 // ROUTES > CONNECTION TO OTHER JS FILES
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require("./lib/Intern");
-const generateHTML =require("./src/generateHTML");
-
-// ROUTES > OUTPUT FILE
-const OUTPUT_DIR = path.resolve(__dirname, "dist")
-const outputPath = path.join(OUTPUT_DIR, "team-dashboard.html");
 
 // ARRAY > TEAM MEMBERS
-const groupQuestions = async (options = []) =>{
+const teamMembers = [];
 
 // PROMPTS > EMPLOYEE QUESTIONS - ALL PROFILES START WITH THIS INFORMATION
 // DOCUMENTATION > NODE INQUIRER - ANSWERS (https://www.npmjs.com/package/inquirer#answers)
@@ -26,88 +22,145 @@ const groupQuestions = async (options = []) =>{
 // DOCUMENTATION > 'LOOPING PROMPTS' DIDN'T WORK WITH THE CODE I HAVE, NOT USING FOR NOW
 // NOTE > PER TUTOR, REMOVE THIS inquirer.registerPrompt('recursive', require('inquirer-recursive'));
 // DOCUMENTATION > NPM WHEN (https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/when.js)
-const addEmployee = [{
-    type: 'input',
-    name: 'name',
-    message: 'What is the employee name?',
-    default: 'Example: Bob'
-},
-{
-    type: 'input',
-    name: 'id',
-    message: 'What is the employee ID number?',
-    default: 'Example: 123456789'
-},
-{
-    type: 'input',
-    name: 'email',
-    message: 'What is the employee email?',
-    default: 'Example: bob@email.com'
-},
-{
-    type: 'input',
-    name: 'officeNumber',
-    message: 'What is the Manager office number?',
-    default: 'Example: 512-512-5112'
-},
-{
-    type: 'list',
-    name: 'role',
-    message: 'Add an employee by selecting an option below:',
-    choices: ['Engineer', 'Intern']
-},
-{
-    type:'input',
-    name: 'github',
-    message: 'What is the Engineer GitHub user name?',
-    when(answers) {
-        return answers.role === 'Engineer'
-    }
-},
-{
-    type: 'input',
-    name: 'school',
-    message: 'What is the name of the School/University for the Intern?',
-    when(answers) {
-        return answers.role === 'Intern'
-    }
-},
-{
-    type: 'confirm',
-    name: 'repeat',
-    message: 'Enter another employee?',
-    default: true
-}
-];
-
-const { repeat, ...answers } = await inquirer.prompt(addEmployee);
-const newEmployee = [...options, answers];
-return repeat ? groupQuestions(newEmployee) : newEmployee;
-};
-
-
-// FUNCTION > MIGRATE DATA FROM PROMPTS INTO NEW FILE
-// NOTE > This function and the functions below were referenced from Challenge 9. My take was "if it works then use it again and modify it for this challenge."
-function writeFile(fileName, groupQuestions) {
-    fs.writeFile(fileName, groupQuestions, function(err) {
-        console.log(fileName)
-        if (err) {
-            return console.log(err)
-        } else {
-            console.log("COMPLETE! New Team Dashboard READY! View the FILE EXPLORER to find your file!")
+const groupQuestions = async () => {
+    const answers = await inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the employee name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('REQUIRED! Example: Bob')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the employee ID number?',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('REQUIRED! Example: 123456789')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the employee email?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('REQUIRED! Example: bob@email.com')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the Manager office number?',
+            validate: officeNumberInput => {
+                if (officeNumberInput) {
+                    return true;
+                } else {
+                    console.log('REQUIRED! Example: 512-512-5112')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Add an employee by selecting an option below:',
+            choices: ['Engineer', 'Intern']
         }
-    })
-}
+    ])
 
-// FUNCTION > INITIALIZE PROGRAM
-const init = async () => {
-    const options = await groupQuestions()
-    return createTeam();
+    if(answers.role === 'Engineer') {
+        const addEngineer = await inquirer
+        .prompt ([
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the Engineer GitHub user name?',
+                validate: githubInput => {
+                    if (githubInput) {
+                        return true;
+                    } else {
+                        console.log('REQUIRED! Example: bobtest')
+                        return false;
+                    }
+                }
+            },
+        ])
+        const newEngineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.officeNumber,
+            addEngineer.github
+        );
+        teamMembers.push(newEngineer);
+    } else if (answers.role === 'Intern') {
+        const addIntern = await inquirer
+        .prompt ([
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What is the name of the School/Unversity for the Intern?',
+                validate: schoolInput  => {
+                    if (schoolInput) {
+                        return true;
+                    } else {
+                        console.log('REQUIRED! Example: UT Austin')
+                        return false;
+                    }
+                }
+            },
+        ])
+        const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.officeNumber,
+            addIntern.school
+        );
+        teamMembers.push(newIntern)
+    }
 };
 
-init();
+// FUNCTION > THIS FUNCTION WILL NAV THROUGH THE PROMPTS AGAIN OR PROMPT THE NEXT FUNCTION 'BUILD TEAM'
+// DOCUMENTATION > ASYNC AND AWAIT (https://www.npmjs.com/package/inquirer#methods)
+async function promptbaseMenu () {
+    await groupQuestions ()
 
-function createTeam () {
-    console.log(options)
-    fs.writeFileSync("./output/new-team-dashboard.html", generateHTML(groupQuestions));
+    const addTeamAnswers = await inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'repeat',
+            message: 'Select an option below:',
+            choices: ['Enter Another Employee', 'Build Team']
+        }
+    ])
+    if (addTeamAnswers.repeat === 'Enter Another Employee') {
+        return promptbaseMenu()
+    }
+    return buildTeam();
 }
+
+function buildTeam () {
+    console.log('Your new team is ready. Review the information and access your new file in the OUTPUT folder.', teamMembers)
+    fs.writeFileSync("./output/new-team-dashboard.html", generateHTML(teamMembers));
+}
+
+promptbaseMenu();
